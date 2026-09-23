00561b34507286e3a60b1fb2095ab5adf14174f5
XaviLG <harold.xavilu21@gmail.com>
2026-09-23 11:12:58 +0200

    Ejercicio 2

--- Tema3/Sesion1/01_EstructuraPrograma/src/main.cpp
+++ Tema3/Sesion1/01_EstructuraPrograma/src/main.cpp
@@ -1,9 +1,26 @@
+// Directivas do preprocesador
 #include <iostream>
-
+#define TOTAL_VALORES 5
 using namespace std;
-
-int main() {
-    // Código 
-    cout << "Ola, mundo!" << endl;
-    return 0;
-}
+//Declaraci´ons de funci´ons e variables globais
+float calcularMedia (float v1, float v2, float v3, float v4, float v5);
+const char autor[] = "Xavier, Lugo";
+// Funci´on principal
+int main()
+    {
+        float v1, v2, v3, v4, v5;
+        cout << "Programa para calcular a media de "
+        << TOTAL_VALORES << " valores" << endl;
+        cout << "Autor: " << autor << endl;
+        cout << "Introduce valores: " << endl;
+        cin >> v1 >> v2 >> v3 >> v4 >> v5;
+        cout << "Media dos valores: " << calcularMedia (v1, v2, v3, v4, v5) << endl;
+        return 0;
+    }
+//Funci´ons definidas polo usuario
+    float calcularMedia (float v1, float v2, float v3, float v4, float v5)
+    {
+        float med;
+        med = (v1 + v2 + v3 + v4 + v5) / TOTAL_VALORES;
+        return med;
+    }
---
+++ Tema3/Sesion1/02_Tipos_Datos/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//Tema 3: Variables e instruci ´ons
+using namespace std;
+int main()
+{
+    int a;
+    const float b = 3.5;
+    char letra = 'x';
+    char frase [20] = "habelas hainas";
+    cout << "Valor da variable enteira a = " << a << endl;
+    cout << "Valor da variable real b = " << b << endl;
+    cout << "valor da variable caracter letra = " << letra << endl;
+    cout << "Valor da cadea de caracteres frase = " << frase << endl;
+    return 0;
+}
